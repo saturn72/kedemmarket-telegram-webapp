6c9342e386369c80dd6674cfb01e2c94b888410c
Roi Shabtai <roi@saturn72.com>
2023-07-28 00:40:13 +0300

    delete cart after placing an order

--- src/.firebase/functions/src/index.ts
+++ src/.firebase/functions/src/index.ts
@@ -3,7 +3,6 @@ import { HttpsError, onCall } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { updateDoc } from "firebase/firestore";
 
 initializeApp();
 
@@ -24,12 +23,24 @@ export const submitOrder = onCall(async req => {
             cartItems: req.data,
             status: 'submitted'
         });
+
     //delete from carts collection
+    const carts = await getFirestore()
+        .collection("carts")
+    const query = await carts
+        .where("userId", "==", req.auth.uid)
+        .limit(1)
+        .get();
+
+    if (query.docs.length > 0) {
+        await query.forEach(async doc => {
+            await doc.ref.delete();
+        });
+    }
 
     logger.debug("end addOrder", { structuredData: true });
     return { data: writeResult.id };
 });
-
 
 export const updateCart = onCall(async (req) => {
     logger.debug("start updateCart", { structuredData: true });
@@ -47,7 +58,6 @@ export const updateCart = onCall(async (req) => {
         .get();
 
     if (query.docs.length == 0) {
-        console.log("inside the loop");
         collection
             .add({
                 cartItems: req.data.items,
