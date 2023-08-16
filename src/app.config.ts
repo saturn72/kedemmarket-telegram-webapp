6f4204e643a060fabee6f58684532bbf779c3d7b
Roi Shabtai <roi@saturn72.com>
2023-08-17 00:34:19 +0300

    fix appconfig/remove redundants

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,11 +2,8 @@ export default defineAppConfig({
     reCaptcha: '6LeGYmwnAAAAAHATMfKr91nVJKL53CDLjYPXRcto',
     firebase: {
         apiKey: "AIzaSyA-511ETL_Jy7jNGC-TNxpD2Dc5Zo883Dg",
-        authDomain: "kedem-market.firebaseapp.com",
-        databaseURL: "https://kedem-market.firebaseio.com",
+        authDomain: "kedem-market.web.app",
         projectId: "kedem-market",
-        storageBucket: "kedem-market.appspot.com",
-        messagingSenderId: "157042094760",
         appId: "1:157042094760:web:9cbf91abe34cf03ce4489d",
         measurementId: "G-N9B0KZT7QR"
     },
