c18eec87de8e086e3fe04ec7fe3692be2aadee91
roi <roi@saturn72.com>
2023-12-28 00:14:15 +0200

    update images src fetching

--- src/components/product/Avatar.vue
+++ src/components/product/Avatar.vue
@@ -4,6 +4,15 @@
             <template #sources>
                 <source :srcset="srcset">
             </template>
+            <template v-slot:placeholder>
+                <div class="d-flex align-center justify-center fill-height">
+                    <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
+                </div>
+            </template>
+            <template v-slot:error>
+                <v-img :src="useAppConfig().defaults.thumbnail">
+                </v-img>
+            </template>
         </v-img>
     </v-avatar>
 </template>
@@ -16,23 +25,15 @@ export default {
         product: { type: Object, default: undefined },
         size: { type: Number, default: 75 }
     },
-    mounted() {
-        this.setSrc();
-        this.setSrcset();
+    async mounted() {
+        this.src = await getProductPrimaryMediaUrl(this.product, "thumbnail");
+        this.srcset = [this.src, await getProductPrimaryMediaUrl(this.product, "image")];
     },
     data: () => {
         return {
-            src: useAppConfig().defaults.thumbnail,
-            srcset: [useAppConfig().defaults.thumbnail]
+            src: undefined,
+            srcset: []
         };
-    },
-    methods: {
-        async setSrc() {
-            this.src = await getProductPrimaryMediaUrl(this.product, "thumbnail");
-        },
-        async setSrcset() {
-            this.srcset = [this.src, await getProductPrimaryMediaUrl(this.product, "image")];
-        }
     }
 }
 </script>
